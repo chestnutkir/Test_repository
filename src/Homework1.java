e99e31bd209b0c7b6820db1f41dcafc2c72e4f2b
chestnutkir <kodlubowsky.kirill@ukr.net>
2023-09-11 00:48:57 +0300

    First commit

---
+++ src/Homework1.java
@@ -0,0 +1,72 @@
+import java.util.Scanner;
+
+public class Homework1 {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);// Створюємо інструмент для зчитування Scanner
+        System.out.println("Розмірність матриці: ");
+        System.out.print("n = ");
+        int n = in.nextInt();//Вводимо кількість рядків
+        System.out.print("m = ");
+        int m = in.nextInt();//Вводимо кількість стовпців
+        long  A[][] = new long [n][];//Виділяємо пам'ять під матриці А, В та результуючу матрицю C
+        for(int i = 0; i < n; i++)
+        {
+          A[i] = new long [m];
+        }
+        long  B[][] = new long [n][];
+        for(int i = 0; i < n; i++)
+        {
+          B[i] = new long [m];
+        }
+        long  C[][] = new long [n][];
+        for(int i = 0; i < n; i++)
+        {
+          C[i] = new long [m];
+        }
+        System.out.println("\n A:");
+        for(int i = 0; i < A.length; i++)
+        {
+          for(int j = 0; j < A[i].length; j++)
+          {
+            A[i][j] = (long) (Math.random()*35-10);//Заповнюємо матрицю А випадковими числами типу long та виводимо його 
+            System.out.print(A[i][j] +" ");
+          }
+          System.out.println();
+        }
+        System.out.println("\n B:");
+        for(int i = 0; i < B.length; i++)
+        {
+          for(int j = 0; j < B[i].length; j++)
+          {
+            B[i][j] = (long) (Math.random()*35-10);//Заповнюємо матрицю B випадковими числами типу long та виводимо його
+            System.out.print(B[i][j] +" ");
+          }
+          System.out.println();
+        }
+        System.out.println("\n C:");
+        for(int i = 0; i < C.length; i++)
+        {
+          for(int j = 0; j < C[i].length; j++)
+          {
+            C[i][j] = A[i][j]^B[i][j];//Виконуємо xor між матрицями А та В
+            System.out.print(C[i][j] +" ");
+          }
+          System.out.println();
+        }
+        long rez = 0;//змінна для пошуку суми найбільших елементів у кожному рядку
+        long max;//Змінна для пошуку найбільшого елемента в рядку
+        for(int i = 0; i < C.length; i++)
+        {
+        	max = C[i][0];
+        	for(int j = 1; j < C[i].length; j++)
+        	{
+        		if (max < C[i][j])
+        		{
+        			max = C[i][j];//Пошук максимального значення у кожному рядку
+        		}
+        	}
+        	rez += max;//Сума найбільших елементів у кожному рядку 
+        }
+        System.out.print("Rezult = " +rez);
+	}
+}
